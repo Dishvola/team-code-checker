03027c95ffa91ea667304943146952257a85e94a
Dishvola <andvasyanov@gmail.com>
2021-03-05 18:15:01 +0200

    add initial files

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # team-code-checker
 Check your code on via "git pre-commit hook" by running PHP Code Sniffer and ESLint
+
+# Istructions
+
+1) Check and install ESLint. For more info https://eslint.org/docs/user-guide/getting-started, https://www.drupal.org/node/1955232.
+	- Install
+	
+	-- Check if exist
+	
+		"eslint -v"
+		
+	-- Global Install if not exist.
+	
+		"npm install eslint --global"
+		
+	-- Verify
+	
+		"eslint -v"
+
+2) Global Coder install via Composer. (https://www.drupal.org/node/1419988#coder)
+	You should do it once for your system!
+	Coder using for check code in the Drupal 7 and 8/9.
+	Those checks apply to all versions of Drupal, so you can use Coder (for example 8.3.x) to check Drupal 7 code.
+	So you should do only this sections.
+	- Install Coder and PHPCS with Composer
+	
+	    -- Global Coder Install
+	
+		    "composer global require drupal/coder"
+		    "export PATH="$PATH:$HOME/.composer/vendor/bin"" (You can skip this step if your $PATH variable contain path to the Composer dir.)
+
+	    -- Register Coder Standards
+	    --- Composer Installer Plugin
+		    
+			        "composer global require dealerdirect/phpcodesniffer-composer-installer"
+
+		--- Manually Set Installed Paths
+		
+			"phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer"
+
+		--- Verify Registered Standards
+		
+			"phpcs -i"
+			
+		Result like - "The installed coding standards are PEAR, Zend, PSR2, MySource, Squiz, PSR1, PSR12, Drupal and DrupalPractice".
+
+3) Go to project root folder where dir ".git/" exist.
+	- Then go to .git/hooks/ folder.
+	
+		    "cd .git/hooks"
+
+	- Clone repo with configurations and scripts.
+	
+		    "git clone git@github.com:Dishvola/team-code-checker.git"
+
+	- Then you have two ways.
+
+	-- If "pre-commit" file doesn't exist.
+		--- Copy example file "cp pre-commit.sample pre-commit"
+		--- Run "chmod +x pre-commit"
+		--- Replace all example shell code with '"$(dirname "$0")"/team-code-checker/team-code-checker.sh' for import our script.
+
+	-- If "pre-commit" file exist.
+		--- Run "chmod +x pre-commit"
+		--- Add string '"$(dirname "$0")"/team-code-checker/team-code-checker.sh' for import our script.
+
+4) Now you can do your work much better! Please try add some code with error to the php or js file for example and then create commit.
---
+++ team-code-checker.sh
@@ -0,0 +1,141 @@
+CROSS_CHECK_RESULT=0
+
+# "$(dirname "$0")"/check-syntax.sh
+# check php/ruby/go syntax
+while read FILE; do
+    # check that file not removed(also can be implemented using --diff-filter)
+    if [[ -f $FILE ]]; then
+        if [[ "$FILE" =~ ^.+(php|html)$ ]]; then
+            php -l "$FILE"
+            if [[ $? -ne 0 ]]; then
+                CROSS_CHECK_RESULT=1
+            fi
+        elif [[ "$FILE" =~ ^.+(rb)$ ]]; then
+            ruby -c "$FILE"
+            if [[ $? -ne 0 ]]; then
+                CROSS_CHECK_RESULT=1
+            fi
+        elif [[ "$FILE" =~ ^.+(go)$ ]]; then
+            gofmt -l -e "$FILE"
+            if [[ $? -ne 0 ]]; then
+                CROSS_CHECK_RESULT=1
+            fi
+        fi
+    fi
+done <<EOT
+    $(git diff --cached --name-only)
+EOT
+
+if [[ $CROSS_CHECK_RESULT -ne 0 ]]; then
+    echo "Aborting commit due to files with syntax errors" >&2
+    exit $CROSS_CHECK_RESULT
+fi
+# =======================
+
+
+# "$(dirname "$0")"/check-debugging-code.sh
+# blackList="console.info\|console.log\|alert\|var_dump\|dsm\|dpm"
+blackList="console.info\|console.log\|var_dump\|dsm\|dpm"
+# CROSS_CHECK_RESULT=0
+while read FILE; do
+    # check that file not removed(also can be implemented using --diff-filter)
+    if [[ -f $FILE ]]; then
+        if [[ "$FILE" =~ ^.+(php|html|js)$ ]]; then
+            RESULT=$(grep -i -m 1 "$blackList" "$FILE")
+            if [[ ! -z $RESULT ]]; then
+                echo "$FILE contains denied word: $RESULT"
+                CROSS_CHECK_RESULT=1
+            fi
+        fi
+fi
+done << EOT
+    $(git diff --cached --name-only)
+EOT
+
+if [[ $CROSS_CHECK_RESULT -ne 0 ]]; then
+    echo "Aborting commit due to denied words"
+    exit $CROSS_CHECK_RESULT
+fi
+# =======================
+
+
+# "$(dirname "$0")"/check-phpcs.sh
+# CROSS_CHECK_RESULT=0
+PROJECT=`php -r "echo dirname(dirname(dirname(realpath('$0'))));"`
+while read FILE; do
+    # check that file not removed(also can be implemented using --diff-filter)
+    if [[ -f $FILE ]]; then
+        if [[ "$FILE" =~ ^.+(php|module|inc|install|test|profile|theme|css|info|txt|md|yml)$ ]]; then
+            FILES="$FILES $PROJECT/$FILE"
+        fi
+fi
+done << EOT
+    $(git diff --cached --name-only)
+EOT
+
+if [[ "$FILES" != "" ]]; then
+    echo "=== Running Code Sniffer. Code standard Drupal. ==="
+    phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml --ignore=node_modules,bower_components,vendor -p $FILES
+    if [[ $? -ne 0 ]]
+    then
+        echo "Error detected!"
+        echo "Run"
+        echo "'phpcbf $FILES'"
+        echo "for automatic fix or fix it manually."
+        CROSS_CHECK_RESULT=1
+    fi
+    echo ""
+    echo ""
+    echo ""
+    echo "=== Running Code Sniffer. Code standard DrupalPractice. ==="
+    phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md,yml --ignore=node_modules,bower_components,vendor -p $FILES
+    if [[ $? -ne 0 ]]
+    then
+        echo "Error detected!"
+        echo "Run"
+        echo "'phpcbf $FILES'"
+        echo "for automatic fix or fix it manually."
+        CROSS_CHECK_RESULT=1
+    fi
+fi
+
+if [[ $CROSS_CHECK_RESULT -ne 0 ]]; then
+    echo "Fix the error before commit!"
+    exit $CROSS_CHECK_RESULT
+fi
+# =======================
+
+
+# "$(dirname "$0")"/check-eslint.sh
+# CROSS_CHECK_RESULT=0
+# PROJECT=`php -r "echo dirname(dirname(dirname(realpath('$0'))));"`
+LINT_CONFIG="$PROJECT/.git/hooks/team-code-checker/eslintrc.json"
+LINT_IGNORE="$PROJECT/.git/hooks/team-code-checker/.eslintignore"
+while read FILE; do
+    # check that file not removed(also can be implemented using --diff-filter)
+    if [[ -f $FILE ]]; then
+        if [[ "$FILE" =~ ^.+(js)$ ]]; then
+            FILES="$FILES $PROJECT/$FILE"
+        fi
+fi
+done << EOT
+    $(git diff --cached --name-only)
+EOT
+
+if [[ "$FILES" != "" ]]; then
+    echo ""
+    echo ""
+    echo "=== Running ESLint. ==="
+    eslint -c $LINT_CONFIG --ignore-path $LINT_IGNORE $FILES
+    if [[ $? -ne 0 ]]
+    then
+        echo "Error detected!"
+        CROSS_CHECK_RESULT=1
+    fi
+fi
+
+if [[ $CROSS_CHECK_RESULT -ne 0 ]]; then
+    echo "Fix the error before commit!"
+    exit $CROSS_CHECK_RESULT
+fi
+# =======================
